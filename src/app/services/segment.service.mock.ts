1be7e6cbe2d0dcaa02976f129bcf7742de2a2466
Mario Herceg <marioherceg655@gmail.com>
2022-11-22 16:12:25 +0100

    Add segment

---
+++ src/app/services/segment.service.mock.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { AnalyticsService } from './analytics.service';
+
+@Injectable({ providedIn: 'root' })
+export class SegmentService implements AnalyticsService {
+  segment: any;
+
+  constructor() {
+    console.log('Using MOCK Segment service!');
+  }
+
+  trackEvent(event: string, properties?: any) {
+    console.log(`[MOCK] Segment event: ${event}`, properties);
+  }
+}
