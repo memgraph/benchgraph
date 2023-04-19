52e4510119a47218a12e04f8e7e4f0d2404ca4f2
Ante Javor <javor.ante@gmail.com>
2023-04-19 11:32:30 +0200

    Update transform.

--- transform.py
+++ transform.py
@@ -4,12 +4,24 @@ import json
 
 working_directory = Path().absolute()
 configs = {
-    "memgraph_cold" : sorted(working_directory.glob("memgraph" + "*" + "cold" + "_*.json")),
-    "memgraph_hot" : sorted(working_directory.glob("memgraph" + "*" + "hot" + "_*.json")),
-    "memgraph_vulcanic" : sorted(working_directory.glob("memgraph"+ "*" + "vulcanic" + "_*.json")),
-    "neo4j_cold" : sorted(working_directory.glob("neo4j"+ "*" + "cold" + "_*.json")), 
-    "neo4j_hot" : sorted(working_directory.glob("neo4j" + "*" + "hot" + "_*.json")),
-    "neo4j_vulcanic" : sorted(working_directory.glob("neo4j" + "*" + "vulcanic" + "_*.json")),
+    "memgraph_cold_12" : sorted(working_directory.glob("memgraph_12" + "*" + "cold" + "_*.json")),
+    "memgraph_hot_12" : sorted(working_directory.glob("memgraph_12" + "*" + "hot" + "_*.json")),
+    "memgraph_vulcanic_12" : sorted(working_directory.glob("memgraph_12"+ "*" + "vulcanic" + "_*.json")),
+    "neo4j_cold_12" : sorted(working_directory.glob("neo4j_12"+ "*" + "cold" + "_*.json")), 
+    "neo4j_hot_12" : sorted(working_directory.glob("neo4j_12" + "*" + "hot" + "_*.json")),
+    "neo4j_vulcanic_12" : sorted(working_directory.glob("neo4j_12" + "*" + "vulcanic" + "_*.json")),
+    "memgraph_cold_24" : sorted(working_directory.glob("memgraph_24" + "*" + "cold" + "_*.json")),
+    "memgraph_hot_24" : sorted(working_directory.glob("memgraph_24" + "*" + "hot" + "_*.json")),
+    "memgraph_vulcanic_24" : sorted(working_directory.glob("memgraph_24"+ "*" + "vulcanic" + "_*.json")),
+    "neo4j_cold_24" : sorted(working_directory.glob("neo4j_24"+ "*" + "cold" + "_*.json")), 
+    "neo4j_hot_24" : sorted(working_directory.glob("neo4j_24" + "*" + "hot" + "_*.json")),
+    "neo4j_vulcanic_24" : sorted(working_directory.glob("neo4j_24" + "*" + "vulcanic" + "_*.json")),
+    "memgraph_cold_48" : sorted(working_directory.glob("memgraph_48" + "*" + "cold" + "_*.json")),
+    "memgraph_hot_48" : sorted(working_directory.glob("memgraph_48" + "*" + "hot" + "_*.json")),
+    "memgraph_vulcanic_48" : sorted(working_directory.glob("memgraph_48"+ "*" + "vulcanic" + "_*.json")),
+    "neo4j_cold_48" : sorted(working_directory.glob("neo4j_48"+ "*" + "cold" + "_*.json")),
+    "neo4j_hot_48" : sorted(working_directory.glob("neo4j_48" + "*" + "hot" + "_*.json")),
+    "neo4j_vulcanic_48" : sorted(working_directory.glob("neo4j_48" + "*" + "vulcanic" + "_*.json")),
 
 }
 mem_cold = sorted(working_directory.glob("memgraph" + "*" + "cold" + "_*.json"))
@@ -30,7 +42,7 @@ for vendor, files in configs.items():
             added = False
             res = TranslateJson.translate(source=file, destination="temp")
             for dataset in data["datasets"]:
-                if dataset["name"] == res["datasets"][0]["name"] and dataset["size"] == res["datasets"][0]["size"]:
+                if dataset["name"] == res["datasets"][0]["name"] and dataset["size"] == res["datasets"][0]["size"] and data["runConfig"]["numberWorkers"] == res["runConfig"]["numberWorkers"]:
                     for workload in res["datasets"][0]["workloads"]:
                         dataset["workloads"].append(workload)
                         added = True
